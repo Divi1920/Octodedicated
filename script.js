8379e22a0c3e38211c8cbb9ac562b1647470f15a
unknown <Divi1920>
2025-10-07 14:47:16 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function generateCode() {
+    let length = document.getElementById("length").value;
+    let characters = "873657580123456789";
+    let code = "";
+
+    for (let i = 0; i < length; i++) {
+        let randomIndex = Math.floor(Math.random() * characters.length);
+        code += characters[randomIndex];
+    }
+
+    document.getElementById("output").textContent = "Your random code: " + code;
+}
